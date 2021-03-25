ace9ffe454a526a9427de428e63cd1f49e9fe650
leatrix <58888415+leatrix@users.noreply.github.com>
2021-03-25 06:45:57 +0000

    9.0.21

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- Leatrix Sounds 9.0.20 (17th March 2021)
+	-- Leatrix Sounds 9.0.21 (25th March 2021)
 	----------------------------------------------------------------------
 
 	--  Create global table
@@ -10,7 +10,7 @@
 	local LeaSoundsLC, LeaSoundsCB, LeaDropList = {}, {}, {}
 
 	-- Version
-	LeaSoundsLC["AddonVer"] = "9.0.20"
+	LeaSoundsLC["AddonVer"] = "9.0.21"
 
 	-- Get locale table
 	local void, Leatrix_Sounds = ...
