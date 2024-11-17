7b5e911e6694953979c82bbed7d4bef751d7402d
leatrix <support@leatrix.com>
2024-11-17 11:32:17 +0000

    1.15.60.alpha.1

--- Leatrix_Sounds_Files.lua
+++ Leatrix_Sounds_Files.lua
@@ -1,6 +1,6 @@
 
 	----------------------------------------------------------------------
-	-- Leatrix Sounds Sound File Listing new
+	-- Leatrix Sounds Sound File Listing
 	----------------------------------------------------------------------
 
 	local void, Leatrix_Sounds = ...
